8813cea0e50f198c09f80a7209272b612164237e
RahulRST <srahuljayaraj@gmail.com>
2023-05-27 11:02:37 +0530

    middleware added

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,11 +3,11 @@ import { Router } from "express";
 import auth from "./auth/auth.route";
 import track from "./track/track.route";
 
-// import { validateUser } from "../middlewares/validate";
+import { validateUser } from "../middleware/validate";
 
 const router: Router = Router();
 
 router.use("/auth", auth);
-router.use("/track", track);
+router.use("/track",validateUser, track);
 
 export default router;
